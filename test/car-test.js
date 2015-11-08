61601e08b06454f25194d0c0d131a9610ac86c81
Kazuki Kasahara <nuderebatsin.roal@gmail.com>
2015-11-08 23:43:10 +0900

    Add loadash to car.js

--- test/car-test.js
+++ test/car-test.js
@@ -6,14 +6,14 @@ console.log(Car);
 describe('Car', function () {
   describe('#constructor', function () {
     it('should return argument propery number', function () {
-      var number = 10;
-      var car = new Car(number);
-      assert.equal(number, car.number);
+      var opts = { number: 10 };
+      var car = new Car(opts);
+      assert.equal(opts.number, car._number);
     });
     it('should return double number', function () {
-      var number = 10;
-      var car = new Car(number);
-      assert.equal(number * 2, car.double);
+      var opts = { number: 10 };
+      var car = new Car(opts);
+      assert.equal(opts.number * 2, car.double);
     });
   });
 });
